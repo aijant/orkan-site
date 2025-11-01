dce21d4cf38e4a747e05ca74889552986f8b1225
Aizhan Tilek <aijanm01@gmail.com>
2025-11-01 18:52:44 +0600

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,14 @@ import { Box, Typography } from "@mui/material";
 export const Footer = () => (
   <Box
     sx={{
-      bgcolor: "primary.main",
+      bgcolor: "#000",
       color: "white",
-      py: 3,
-      mt: 8,
+      py: 8,
       textAlign: "center",
     }}
   >
     <Typography variant="body2">
-      © {new Date().getFullYear()} Xplore Clone. All rights reserved.
+      Copyright © {new Date().getFullYear()} Orkan ELD. All rights reserved.
     </Typography>
   </Box>
 );
